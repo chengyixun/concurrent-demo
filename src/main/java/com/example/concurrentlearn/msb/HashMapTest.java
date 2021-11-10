b2d68b34de54dd69b56e123010cf8f557746fb86
amy <1501212682@qq.com>
2021-11-10 17:19:15 +0800

    add HashMapTest

---
+++ src/main/java/com/example/concurrentlearn/msb/HashMapTest.java
@@ -0,0 +1,20 @@
+package com.example.concurrentlearn.msb;
+
+import java.util.HashMap;
+
+/**
+ * @ClassName: HashMapTest
+ * @Author: amy
+ * @Description: HashMapTest
+ * @Date: 2021/11/10
+ * @Version: 1.0
+ */
+public class HashMapTest {
+  public static void main(String[] args) {
+    //
+      HashMap hashMap = new HashMap();
+      hashMap.put("key1","value1");
+
+
+  }
+}
