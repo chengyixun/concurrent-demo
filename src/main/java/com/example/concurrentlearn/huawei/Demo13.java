467f09222d742be7b61312f24e7ab20dae3a8dfd
amy <1501212682@qq.com>
2021-11-17 13:04:34 +0800

    add demo

--- src/main/java/com/example/concurrentlearn/huawei/Demo13.java
+++ src/main/java/com/example/concurrentlearn/huawei/Demo13.java
@@ -1,23 +1,46 @@
 package com.example.concurrentlearn.huawei;
 
-import java.util.HashMap;
-import java.util.Hashtable;
-import java.util.Map;
+import java.util.Scanner;
 
-/**
- * @Author: wangyu
- * @Date: Created 2020-06-13 11:31
- * @Description:
- * @Modified By:
- */
+/** @Author: wangyu @Date: Created 2020-06-13 11:31 @Description: @Modified By: */
 public class Demo13 {
 
-    public static void main(String[] args) {
-        Map map = new HashMap();
-
-        Hashtable<Object, Object> hashtable = new Hashtable<>();
-      //  hashtable.put();
-
-
+  public static void main(String[] args) {
+    /* int sum = 0;
+    for (int i = 1; i < 21; i++) {
+      if (i % 2 == 0) {
+        sum = sum - i;
+      } else {
+        sum = sum + i;
+      }
     }
+    System.out.println(sum);*/
+
+    Scanner sc = new Scanner(System.in);
+    while (sc.hasNext()) {
+      int i = sc.nextInt();
+
+      System.out.println(getMethod(i));
+    }
+  }
+
+  private static String getMethod(int time) {
+    // 声明三个变量
+    int hour = 0, min = 0, sec = 0;
+    // 计算出小时
+    hour =  time / (60 * 60);
+    // 计算出分钟
+    min = (time - hour * 3600) / 60;
+    // 计算出秒数
+    sec =  time - hour * 3600 - min * 60;
+
+    return new StringBuffer()
+        .append(hour)
+        .append("时")
+        .append(min)
+        .append("分钟")
+        .append(sec)
+        .append("秒")
+        .toString();
+  }
 }
---
+++ src/main/java/com/example/concurrentlearn/huawei/Demo14.java
@@ -0,0 +1,23 @@
+package com.example.concurrentlearn.huawei;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @ClassName: Demo14
+ * @Author: amy
+ * @Description: demo14
+ * @Date: 2021/11/16
+ * @Version: 1.0
+ */
+public class Demo14 {
+  public static void main(String[] args) {
+    // 基本数据类型的数组转换成集合
+
+      // //将数组转换成集合,数组必须是引用数据类型
+
+      int[]  array = {2, 3, -8, -1, 2, 4, -2, 3};
+
+      //List<int[]> ints = Arrays.asList(array).
+  }
+}
